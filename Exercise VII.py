0cb87623bfb38c362967cf64d0e027f98ee02026
JJkittipop <jj-love-myz@hotmail.com>
2024-08-18 21:49:08 +0700

    number

---
+++ Exercise VII.py
@@ -0,0 +1,13 @@
+a = int(input("number: "))
+print(a,end=" ")
+if(a%2==1):
+   print ("เลขคี่")
+else:
+   print ("เลขคู่")
+
+for a in range(21,40+1):
+    print(a,end=" ")
+    if(a%2==1):
+        print ("เลขคี่")
+    else:
+        print ("เลขคู่")
