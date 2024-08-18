b79df2de56cf2fd278d633164f5a452481ad45cd
JJkittipop <jj-love-myz@hotmail.com>
2024-08-18 14:24:02 +0700

    python conditions

---
+++ python4.py
@@ -0,0 +1,47 @@
+""""""
+#
+#part: python conditions
+#
+""""""
+a = 200
+b = 500
+if a > b:
+  print("a < b is true")
+  print("a < b is true")
+  print("a < b is true")
+elif a > b:
+   print("a > b is True")
+elif a == b:
+   print("a == b is True")
+elif a >= b:
+   print("a >= b is True")
+else :
+   print("Nothing")
+
+if a > b: print("a > b is True")
+
+print("A") if a < b else print("B")
+
+a = 200 
+b = 50 
+c = 500
+if (a > b) and (c > a):
+    print("Both is True")
+if a > b or a > c:
+   print("Some cond is True")
+if not (a > b):
+ print("False")
+if b > a:
+   pass
+
+x = 19
+if x> 10:
+   print("Lets go")
+   if x> 20:
+       print("x >20 is True")   
+   if x> 20:
+       print("x >20 is True")   
+   if x> 20:
+       print("x >20 is True")   
+   if x> 20:
+       print("x >20 is True")   
